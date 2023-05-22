52e03b384540a753d24fa2a860ae57ee40ede085
vijayskk <vijayskk007@gmai.com>
2023-05-22 19:25:22 +0530

    First one

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 def warn(*args, **kwargs):
     pass
 import warnings
+import platform
 warnings.warn = warn
-
-
 import joblib
-
+import os
 model = joblib.load('model.joblib')
 
-# pred = model.predict([[0,0.6,1,0.6,1,0,0,0,0,10,10]])
-# print(pred)
 
 
 def askYN(qn):
@@ -36,10 +33,34 @@ def calcDigits(string):
             pass
     return digit
 
-username = input("What is the username?")
-fullname = input("What is the fullname?")
+def clearTerminal():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def printUnderlined(text):
+    print(f"\033[4m{text}\033[0m")
+
+def printNewline(n):
+    for i in range(n):
+        print("\n")
 
 
+clearTerminal()
+
+printUnderlined("How it works")
+
+printNewline(1)
+
+print("This program will predict a social media account fake or not. All you have to do is give answers for the following questions.\nPlease note that the the model only have 90% accuracy. So don't take it as a final destination.")
+printNewline(3)
+input("Press enter to continue...")
+
+clearTerminal()
+
+username = input("What is the username? :")
+fullname = input("What is the fullname? :")
 pp = askYN("Is there a profile picture? (y/n) :")
 numbylenu = calcDigits(username) / len(username)
 fullnamewords = len(fullname.split(' '))
@@ -48,15 +69,17 @@ nameeqfull = username == fullname
 dlength = len(input("Copy paste the description: ").replace(" ",""))
 haveURL = askYN("Is there an external link or url in the description? (y/n): ")
 isPrivate = askYN("Is that a private account? (y/n): ")
-posts = askInt("How many posts have?")
-followers = askInt("How many followers have?")
-follows = askInt("How many following have?")
+posts = askInt("How many posts have? :")
+followers = askInt("How many followers have? :")
+follows = askInt("How many following have? :")
 
 pred = model.predict([[pp,numbylenu,fullnamewords,numbylenf,nameeqfull,dlength,haveURL,isPrivate,posts,followers,follows]])
-print(pred)
+
+printNewline(2)
 
 if pred[0] == 1:
     print("The account is fake")
 else:
     print("The account is NOT fake")
 
+printNewline(2)
